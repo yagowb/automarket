f2485da355bace39d757bd4144289c1d9f75db86
yago <yagodev3@gmail.com>
2026-02-26 23:30:03 -0300

    fix(server): migrate eslint config to flat config format

---
+++ server/eslint.config.js
@@ -0,0 +1,17 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
